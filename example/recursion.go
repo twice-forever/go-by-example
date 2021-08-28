bd8751e979d0958b54c5113a4db9b8be553ac9e0
twice-forever <bnn0202@outlook.com>
2021-08-28 12:10:53 +0800

    feat: 增加递归例子

---
+++ example/recursion.go
@@ -0,0 +1,15 @@
+package example
+
+import "fmt"
+
+// 递归调用
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func Recursion() {
+	fmt.Println(fact(7))
+}
--- main.go
+++ main.go
@@ -19,4 +19,5 @@ func main() {
 	example.MultipleReturnValues()
 	example.VariadicFunction()
 	example.Closures()
+	example.Recursion()
 }
